73698ebab57c58ae5df437408a8459366417686c
ForrestYankey <forrestyankey@gmail.com>
2024-01-23 19:28:04 -0500

    Create kata practice class for Chase interview

---
+++ java/src/main/java/ChasePrep.java
@@ -0,0 +1,15 @@
+public class ChasePrep {
+
+    /**  given an array of numbers find the largest sum of continuous numbers.
+     * */
+    public static void main(String[] args) {
+      String helloWorld = helloWorld();
+
+        System.out.println(helloWorld);
+    }
+
+    private static String helloWorld() {
+        String string = "Hello World!";
+        return string;
+    }
+}
