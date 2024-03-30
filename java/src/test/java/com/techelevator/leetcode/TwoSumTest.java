c4938cc26b507af9c884075a22e0e52e1e311b86
ForrestYankey <forrestyankey@gmail.com>
2024-03-30 01:18:35 -0400

    started twoSums

---
+++ java/src/test/java/com/techelevator/leetcode/TwoSumTest.java
@@ -0,0 +1,32 @@
+package com.techelevator.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    private TwoSum twoSum = new TwoSum();
+
+    @Test
+    public void testTwoSum_GivenArrayOfTwoNumbersAndTarget_Returns0And1() {
+        int[] nums = new int[]{1, 2};
+        int[] expectedNums = new int[]{0, 1};
+        int target = 3;
+
+        int[] results = twoSum.twoSum(nums, target);
+
+        assertEquals(results, expectedNums);
+    }
+
+    @Test
+    public void testTwoSum_GivenArrayOfThreeNumbersAndTarget_ReturnTwoNumbersThatEqualTarget() {
+        int[] nums = new int[]{1, 2, 3};
+        int[] expectedNums = new int[]{0, 1};
+        int target = 3;
+
+        int[] results = twoSum.twoSum(nums, target);
+
+        assertArrayEquals(results, expectedNums);
+    }
+}
