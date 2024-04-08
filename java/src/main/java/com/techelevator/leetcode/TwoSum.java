4d9b9c03dc9a06bd9cf5a134a67b745ad1645786
ForrestYankey <forrestyankey@gmail.com>
2024-04-08 08:54:50 -0400

    refactored to accept zeros and negatives

--- java/src/main/java/com/techelevator/leetcode/TwoSum.java
+++ java/src/main/java/com/techelevator/leetcode/TwoSum.java
@@ -3,10 +3,10 @@ package com.techelevator.leetcode;
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers
  * such that they add up to target.
- *
+ * <p>
  * You may assume that each input would have exactly one solution, and you may not use the
  * same element twice.
- *
+ * <p>
  * You can return the answer in any order.
  */
 public class TwoSum {
@@ -14,12 +14,10 @@ public class TwoSum {
         int[] twoNums = new int[2];
         for (int i = 0; i < nums.length; i++) {
             int currentNum = nums[i];
-            if (currentNum < target) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (currentNum + nums[j] == target) {
-                        twoNums[0] = i;
-                        twoNums[1] = j;
-                    }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (currentNum + nums[j] == target) {
+                    twoNums[0] = i;
+                    twoNums[1] = j;
                 }
             }
         }
--- java/src/test/java/com/techelevator/leetcode/TwoSumTest.java
+++ java/src/test/java/com/techelevator/leetcode/TwoSumTest.java
@@ -43,4 +43,26 @@ class TwoSumTest {
 
         assertArrayEquals(results, expectedNums);
     }
+
+    @Test
+    public void testTwoSum_GivenArrayOfFourNumbersWithZerosAndTarget_ReturnTwoNumbersThatEqualTargetLocation() {
+        int[] nums = new int[]{0, 4, 3, 0};
+        int[] expectedNums = new int[]{0, 3};
+        int target = 0;
+
+        int[] results = twoSum.twoSum(nums, target);
+
+        assertArrayEquals(results, expectedNums);
+    }
+
+    @Test
+    public void testTwoSum_GivenArrayOfFourNegativeNumbersAndNegativeTarget_ReturnTwoNumbersThatEqualTargetLocation() {
+        int[] nums = new int[]{-1, -2, -3, -4, -5};
+        int[] expectedNums = new int[]{2, 4};
+        int target = -8;
+
+        int[] results = twoSum.twoSum(nums, target);
+
+        assertArrayEquals(results, expectedNums);
+    }
 }
