09e0531e69f50bafcee989e782edcde12baa5471
ForrestYankey <forrestyankey@gmail.com>
2024-04-09 12:13:38 -0400

    add other solution with modulo

--- java/src/main/java/com/techelevator/leetcode/PalindromeNumber.java
+++ java/src/main/java/com/techelevator/leetcode/PalindromeNumber.java
@@ -8,21 +8,38 @@ import java.util.ArrayList;
  */
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
-        String nums = String.valueOf(x);
-        ArrayList<String> numsArray = new ArrayList<>();
-        int n = nums.length();
+//        String nums = String.valueOf(x);
+//        ArrayList<String> numsArray = new ArrayList<>();
+//        int n = nums.length();
+//
+//        for (int i = 0; i < n; i++) {
+//            numsArray.add(String.valueOf(nums.charAt(i)));
+//        }
+//        //check if palindrome
+//        for (int i = 0; i < n; i++) {
+//            String currentNum = numsArray.get(i);
+//            String pointer = numsArray.get((n-1) - i);
+//            if (!currentNum.equals(pointer)) {
+//                return false;
+//            }
+//        }
+//        return true;
 
-        for (int i = 0; i < n; i++) {
-            numsArray.add(String.valueOf(nums.charAt(i)));
+
+        //Reversing the entire number
+        if (x < 0) { //check for negative
+            return false;
         }
-        //check if palindrome
-        for (int i = 0; i < n; i++) {
-            String currentNum = numsArray.get(i);
-            String pointer = numsArray.get((n-1) - i);
-            if (!currentNum.equals(pointer)) {
-                return false;
-            }
+
+        long reversed = 0; //where we store the reversed value of x
+        long temp = x; //placeholder to manipulate the input num without modifying the original value
+
+        while (temp != 0) {
+            int digit = (int) (temp % 10);
+            reversed = reversed * 10 + digit;
+            temp /= 10;
         }
-        return true;
+
+        return (reversed == x);
     }
 }
